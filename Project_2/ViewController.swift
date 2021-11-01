fccaf2645e6508b5b9c286cd03bf0a0c4a614114
MohammedMabruk <Mohammed.Mabruk.med.s@tuwaiq.edu.sa>
2021-11-01 12:44:18 +0300

    Initial Commit

---
+++ Project_2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project_2
+//
+//  Created by Mohammed Mabruk on 26/03/1443 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
